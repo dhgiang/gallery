9815b4f1c9e067881bfb3e33f8036326548058ac
MBA512 <mba512@MBA512s-MacBook-Air.local>
2018-04-10 03:00:37 -0400

    remove commented out code on db config

--- config/database.js
+++ config/database.js
@@ -1,6 +1,5 @@
 if(process.env.NODE_ENV === 'production') {
     module.exports = {mongoURI: 'mongodb://duc:duc@ds139919.mlab.com:39919/gallery-prod'}
 } else {
-    // module.exports = {mongoURI: 'mongodb://duc:duc@ds139919.mlab.com:39919/gallery-prod'}
     module.exports = {mongoURI: 'mongodb://localhost:27017/gallery'}
 }
