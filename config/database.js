8a0526d8ada7512909cab03bb2b9bbce67565387
MBA512 <mba512@MBA512s-MacBook-Air.local>
2018-04-10 03:01:36 -0400

    minor changes

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production') {
     module.exports = {mongoURI: 'mongodb://duc:duc@ds139919.mlab.com:39919/gallery-prod'}
-} else {
+} else {    
     module.exports = {mongoURI: 'mongodb://localhost:27017/gallery'}
 }
